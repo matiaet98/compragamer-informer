f2c9697ae72c19a2c42365f938aaebf5b7cc5a1f
matiaet98 <matias.exequiel.estevez@gmail.com>
2020-09-24 17:54:39 -0300

    function ask template

--- main.py
+++ main.py
@@ -8,5 +8,12 @@ bot = telebot.TeleBot(settings.TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
 def start_help_handler(message):
-    bot.reply_to(message, "registered!")
+    bot.reply_to(message, "hello!")
 
+
+@bot.message_handler(commands=['ask'])
+def ask_handler(message):
+    bot.reply_to(message, message.text)
+
+
+bot.polling()
