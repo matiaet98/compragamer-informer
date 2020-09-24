66838dc9a7987f0a103df80e8c0ebb49f8e84b52
matiaet98 <matias.exequiel.estevez@gmail.com>
2020-09-24 19:49:32 -0300

    progress with the main function

--- main.py
+++ main.py
@@ -2,18 +2,35 @@
 
 import settings
 import telebot
+import time
+import threading
+import logging
 
+logging.basicConfig(level=logging.INFO)
 bot = telebot.TeleBot(settings.TOKEN)
+chatids = []
 
 
 @bot.message_handler(commands=['start', 'help'])
 def start_help_handler(message):
+    logging.debug(msg="start received from {id}".format(id="message.chat.id"))
     bot.reply_to(message, "hello!")
 
 
 @bot.message_handler(commands=['ask'])
 def ask_handler(message):
-    bot.reply_to(message, message.text)
+    logging.debug(msg="ask received from {id}".format(id="message.chat.id"))
+    chatids.append(message.chat.id)
+    bot.reply_to(message, "ok")
 
 
-bot.polling()
+def botpolling():
+    logging.info("Start Polling")
+    bot.polling()
+
+
+threading.Thread(target=botpolling).start()
+while True:
+    for id in chatids:
+        bot.send_message(id, "okokokok")
+    time.sleep(int(settings.INTERVAL))
